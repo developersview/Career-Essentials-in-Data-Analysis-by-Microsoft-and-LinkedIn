c029653c6120d86e2c67b6291a0081fda97fe0ff
developersview <pcslg1998@gmail.com>
2024-05-25 02:53:09 +0530

    updated self and cross join query

--- _2_Learning Data Analytics - 1 Foundations/Ch03/query2.sql
+++ _2_Learning Data Analytics - 1 Foundations/Ch03/query2.sql
@@ -11,15 +11,29 @@ RIGHT JOIN [Sales].[SalesOrderDetail] AS sod
 ON p.ProductID = sod.ProductID;
 
 --INNER JOIN
-SELECT TOP 2 *
-FROM [Production].[Product] AS p
-INNER JOIN [Sales].[SalesOrderDetail] AS sod
+SELECT   p.ProductID
+		,p.ProductNumber
+		,p.Color
+		,p.Name
+		,p.StandardCost
+		,sod.SalesOrderID
+		,sod.OrderQty
+		,sod.UnitPrice
+FROM [Production].[Product] p
+INNER JOIN [Sales].[SalesOrderDetail] sod
 ON p.ProductID = sod.ProductID;
 
 --FULL JOIN
-SELECT TOP 2 *
-FROM [Production].[Product] AS p
-FULL JOIN [Sales].[SalesOrderDetail] AS sod
+SELECT  p.ProductID
+		,p.ProductNumber
+		,p.Color
+		,p.Name
+		,p.StandardCost
+		,sod.SalesOrderID
+		,sod.OrderQty
+		,sod.UnitPrice
+FROM [Production].[Product] p
+FULL JOIN [Sales].[SalesOrderDetail] sod
 ON p.ProductID = sod.ProductID;
 
 --CROSS JOIN
@@ -45,3 +59,16 @@ FROM [Production].[Product] p
 LEFT JOIN [Production].[ProductSubcategory] psc
 ON p.ProductSubcategoryID = psc.ProductSubcategoryID
 WHERE p.ProductSubcategoryID is NULL;
+
+--SELF JOIN
+SELECT sp1.BusinessEntityID
+	,sp1.TerritoryID
+	,sp1.SalesQuota
+	,sp1.Bonus
+	,sp2.BusinessEntityID
+	,sp2.TerritoryID
+	,sp2.SalesQuota
+	,sp2.Bonus
+FROM [Sales].[SalesPerson] sp1
+INNER JOIN [Sales].[SalesPerson] sp2
+ON sp1.BusinessEntityID = sp2.BusinessEntityID;
